92de3e89a56f8c9c49916c4dc51ca743e98344d3
Ranjesh9525 <product.circ@i10ai.com>
2025-04-23 07:25:16 +0530

    header_static

--- src/components/checkouts/AdditionalServicesCard.tsx
+++ src/components/checkouts/AdditionalServicesCard.tsx
@@ -35,7 +35,7 @@ const AdditionalServicesCard: React.FC<AdditionalServicesCardProps> = ({ onSubmi
               checked={warehouseNeeded}
               onChange={() => setWarehouseNeeded(!warehouseNeeded)}
             />
-            <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-orange-300 rounded-full peer peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-orange-500"></div>
+            <div className="w-11 h-6 bg-gray-400 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-orange-300 rounded-full peer peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-orange-500"></div>
           </label>
         </div>
 
@@ -48,7 +48,7 @@ const AdditionalServicesCard: React.FC<AdditionalServicesCardProps> = ({ onSubmi
               checked={logisticsNeeded}
               onChange={() => setLogisticsNeeded(!logisticsNeeded)}
             />
-            <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-orange-300 rounded-full peer peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-orange-500"></div>
+            <div className="w-11 h-6 bg-gray-400 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-orange-300 rounded-full peer peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-orange-500"></div>
           </label>
         </div>
       </div>
--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -47,6 +47,9 @@ export default function Header({ user }: HeaderProps) {
     "Wire Mesh & Gratings",
     "Containers",
     "Papers",
+    "Gratings",
+    "Clothes",
+    "Tools",
   ]
 
   function handleAuthSuccess() {
@@ -97,7 +100,7 @@ export default function Header({ user }: HeaderProps) {
     <header className="w-full bg-white shadow-sm">
       {/* Top Navigation - Fixed */}
       <div className="fixed top-0 left-0 right-0 z-50 bg-white shadow-sm">
-        <div className="container mx-auto px-4 sm:px-6 lg:px-8 py-4">
+        <div className="container mx-auto px-2 sm:px-4 lg:px-6 py-2">
           <div className="flex items-center justify-between">
             {/* Logo */}
             <Link href="/" className="flex items-center gap-2 mr-auto">
@@ -182,11 +185,11 @@ export default function Header({ user }: HeaderProps) {
       </div>
 
       {/* Empty space to compensate for the fixed header */}
-      <div className="h-[72px]"></div>
+      <div className="h-[50px]"></div>
 
       {/* Categories Navigation */}
       <div className="bg-[#004D40] text-white overflow-x-auto">
-        <div className="container mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="container mx-auto px-3 sm:px-5 lg:px-7">
           <div className="flex items-center py-3 space-x-4 sm:space-x-8">
             <span className="text-sm whitespace-nowrap">Explore:</span>
             {categories.map((category, index) => (
