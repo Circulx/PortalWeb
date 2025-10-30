dfc52cab3520c5c87ebe8c7a2b5dd8e43f0a513b
Circulx <product.circ@i10ai.com>
2025-10-30 19:37:51 +0530

    Update Footer.tsx

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -173,7 +173,7 @@ export default function Footer() {
                   </svg>
                   <span className="sr-only">Instagram</span>
                 </Link>
-                <Link href="/twitter" className="text-white hover:text-gray-300" target="_blank" aria-label="Youtube">
+                <Link href="https://youtube.com/@ind2b?si=5RNIa72q-BX30sjL" className="text-white hover:text-gray-300" target="_blank" aria-label="Youtube">
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
                     <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z" />
                   </svg>
