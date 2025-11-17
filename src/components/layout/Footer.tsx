5e20cd3944c63c30ce05d54f0f1e13c299089d5e
Circulx <product.circ@i10ai.com>
2025-11-17 18:59:24 +0530

    updated some hyperlink

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -72,10 +72,10 @@ export default function Footer() {
               <Link href="/careers" className="block text-white hover:text-gray-300">
                 Careers
               </Link>
-              <Link href="/press" className="block text-white hover:text-gray-300">
+              <Link href="/" className="block text-white hover:text-gray-300">
                 Press
               </Link>
-              <Link href="/articles" className="block text-white hover:text-gray-300">
+              <Link href="/" className="block text-white hover:text-gray-300">
                 Articles
               </Link>
             </div>
