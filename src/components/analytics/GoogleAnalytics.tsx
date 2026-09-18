bbb44f8ddeb0b6c07688e3c3ab35de77439d62e4
Circulx <product.circ@i10ai.com>
2026-09-18 10:45:40 +0530

    Update GoogleAnalytics.tsx

--- src/components/analytics/GoogleAnalytics.tsx
+++ src/components/analytics/GoogleAnalytics.tsx
@@ -6,7 +6,7 @@ import { GA_TRACKING_ID } from "@/lib/gtag"
 export default function GoogleAnalytics() {
   return (
     <>
-      <Script strategy="afterInteractive" src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} />
+      <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} />
       <Script
         id="gtag-init"
         strategy="afterInteractive"
